f25b6aa879ae0b0b78af5cca54feb5b82475dfe6
Daniele Borra <daniele.borra.93@gmail.com>
2016-12-08 15:24:12 +0100

    Page class methods definitions and file loading

--- rails.js
+++ rails.js
@@ -9,17 +9,25 @@ class Rails {
 		// Proprieties from parameters
 		this.manageAnchors = options.manageAnchors || true;
 		this.contentSelector = options.contentSelector || '#rails-page';
+		this.baseDirectory = options.baseDirectory || '/pages/';
+		this.baseExtension = options.baseExtension || '.html';
 
 		// Perform all the required tasks in options
 		// or setup variables
-		this.conteiner = document.querySelectorAll( this.contentSelector )[0];
+		this.container = document.querySelectorAll( this.contentSelector )[0];
 		this.manageAnchors && this.handleAnchors();
 
 		// Register a path for each path in paths
-		for (var i = 0; i < paths.length; i++) {
-			var path = paths[i];
-			this.registerPath( path );
-		}
+		paths.forEach((current, index) => {
+			if (typeof current == 'object') {
+				if (typeof current.path == 'undefined') throw "No propriety path in object";
+				if (typeof current.page == 'undefined') throw "No propriety page in object";
+				this.registerPath( current.path, current.page );
+			}
+			if (typeof current == 'string') {
+				this.registerPath( current );
+			}
+		} );
 
 		typeof callback == 'function' && callback();
 	}
@@ -35,17 +43,22 @@ class Rails {
 		var page = parts[4];
 		var found = false;
 		this.registered.forEach( element => { if( element.path == page ) found = element; } );
-		if( found )
+		if( found ) {
 			window.history.pushState({ url: page }, page.toUpperCase(), page);
-		else
+			found.page.loadPage();
+		} else
 			throw "Loading a non registered path";
 	}
 
-	registerPath( path ) {
-		var page = new Page();
+	registerPath( path, page ) {
+		if( !page ) {
+			var pageref = new Page( this.baseDirectory + path + this.baseExtension, this );
+		} else {
+			var pageref = page;
+		}
 		this.registered.push( {
 			path: path,
-			page: page
+			page: pageref
 		} );
 	}
 
@@ -68,9 +81,14 @@ class Rails {
 }
 
 class Page {
-	contruct( file ) {
+	constructor( file, rails ) {
+		if( typeof file == 'undefined' ) throw "Undefined file propriety";
+		if( typeof rails == 'undefined' ) throw "Undefined rails instance";
 		// Get the file path for the html to laod
 		this.file = file;
+		// Rails param is the reference to the rails instance
+		// witch the page belongs to
+		this.rails = rails;
 	}
 
 	loadPage() {
@@ -78,14 +96,22 @@ class Page {
 			url: this.file,
 			method: 'GET'
 		}, (error, response, body) => {
-			console.log(error);
-			console.log(response);
-			console.log(body);
+			if( error ) throw error;
+			this.rails.container.innerHTML = body;
 		});
 	}
 
 	animateIn() {
 		// Fancy animation for getting the page in here
+	}
+
+	animateOut() {
+		// Fany animation out here
+	}
+
+	remove() {
+		// Remove the page HTML from the rails container
+		this.rails.container.innerHTML = '';
 	}
 }
 
